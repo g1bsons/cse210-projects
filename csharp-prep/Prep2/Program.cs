a5a8780868f718ec820110f8ffacc455674b1d50
gibson <gibsoncsmith1@gmail.com>
2026-06-10 17:42:20 -0600

    Wrote a grade output program including the + and the -

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,79 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string percentage = Console.ReadLine();
+        int percent = int.Parse(percentage);
+
+        string letter = "";
+
+        if (percent >= 97)
+        {
+            letter = "A+";
+        }
+        else if (percent >=94)
+        {
+            letter = "A";
+        }
+        else if (percent >=90)
+        {
+            letter = "A-";
+        }
+        else if (percent >= 87)
+        {
+            letter = "B+";
+        }
+        else if (percent >=84)
+        {
+            letter = "B";
+        }
+        else if (percent >=80)
+        {
+            letter = "B-";
+        }
+        else if (percent >= 77)
+        {
+            letter = "c+";
+        }
+        else if (percent >=74)
+        {
+            letter = "c";
+        }
+        else if (percent >=70)
+        {
+            letter = "c-";
+        }
+        else if (percent >= 67)
+        {
+            letter = "D+";
+        }
+        else if (percent >=64)
+        {
+            letter = "D";
+        }
+        else if (percent >=60)
+        {
+            letter = "D-";
+        }
+        else
+        {
+            letter = "F";
+        }
+        
+        Console.WriteLine($"Your grade is _{letter}_");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congrats, you passed the class");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you didnt pass. Better study more next time");
+        }
+
     }
 }
