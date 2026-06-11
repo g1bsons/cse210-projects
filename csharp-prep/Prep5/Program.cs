e039cab08a6492335ba90399fba6abf500d5e786
gibson <gibsoncsmith1@gmail.com>
2026-06-10 19:59:12 -0600

    get and attach username to the display message

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,15 +1,25 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
         DisplayWelcome();
+        string name = get_username();
 
     }
       
-        static void DisplayWelcome()
+    static void DisplayWelcome()
     {
         Console.WriteLine("Welcome to the Program!");
     }
+
+    static string get_username()
+    {
+        Console.WriteLine("Whats your name?");
+        string name = Console.ReadLine();
+        return name;
+    }
+
 }
