f23f40be0733f488b4652ebcd73f3dcf5ad1804d
gibson <gibsoncsmith1@gmail.com>
2026-06-10 19:31:58 -0600

    completed steo 1 and calculated the sum

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int users_number;
+
+        Console.WriteLine("Please enter a series of numbers. When you are finished please type'0' to finish the series.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            users_number = int.Parse(Console.ReadLine());
+
+            if (users_number != 0)
+            {
+                numbers.Add(users_number);
+            }
+
+        } while (users_number != 0);
+
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
     }
 }
