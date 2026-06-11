44c0faedb48d9f1ef49be56e88d75261abaccbda
gibson <gibsoncsmith1@gmail.com>
2026-06-10 19:53:14 -0600

    displayed the welcome to the program message

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+    }
+      
+        static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
     }
 }
