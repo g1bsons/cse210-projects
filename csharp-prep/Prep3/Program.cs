4afe2eeacc83b57d88b861a78e2f2195d56b7ac2
gibson <gibsoncsmith1@gmail.com>
2026-06-10 19:06:51 -0600

    looped the guesses and added random number generation

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,25 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is the magic number?");
-        int magic_number = int.Parse(Console.ReadLine());
+        // Console.WriteLine("What is the magic number?");
+        // int magic_number = int.Parse(Console.ReadLine());
+        Random random_number = new Random();
+        int magic_number = random_number.Next(1,101);
 
         int guess = -1;
 
-        Console.WriteLine("What is your guess?");
-        guess = int.Parse(Console.ReadLine());
+        while (guess != magic_number)
+        {
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
 
-        if (magic_number > guess)
-        {
-            Console.WriteLine("Higher");    
-        }
-        else if (magic_number < guess)
-        {
-            Console.WriteLine("Lower");
-        }
-        else
-        {
-            Console.WriteLine("Correct!!");
+            if (magic_number > guess)
+            {
+                Console.WriteLine("Higher");    
+            }
+            else if (magic_number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Correct!!");
+            }
         }
     }
 }
