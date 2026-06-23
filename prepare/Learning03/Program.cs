67a9fee227fdf13da1e32012ea53b2b7d3b6ee29
gibson <gibsoncsmith1@gmail.com>
2026-06-23 16:48:22 -0600

    added a random tester function to the program class to test the fraction class

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
-    }
+        Fraction f = new Fraction();
+        Random random = new Random();
+
+        for (int i = 1; i <= 20; i++)
+        {
+            int top = random.Next(1, 10);
+            int bottom = random.Next(1, 10);
+
+            f.SetTop(top);
+            f.SetBottom(bottom);
+
+            Console.WriteLine($"Fraction {i}: string: {f.GetFractionString()} Number: {f.GetDecimalValue()}");
+}
+        }
 }
