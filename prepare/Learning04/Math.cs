f6ca7e841cfcd6a0585d3fc37f60007315efbdcb
gibson <gibsoncsmith1@gmail.com>
2026-06-30 17:42:37 -0600

    Started building math.cs class

--- prepare/Learning04/Math.cs
+++ prepare/Learning04/Math.cs
@@ -1,2 +1,13 @@
 using System;
 
+// Added math to inherit from Assignments
+public class Math Math : Assignments
+{
+    // Added the attributes as private variables
+    private string _textbookSection;
+    private string _problems;
+
+
+
+
+}
