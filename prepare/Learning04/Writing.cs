8e7dd7dd6179d4f15d45739ccc7cb0015f306155
gibson <gibsoncsmith1@gmail.com>
2026-06-30 17:59:43 -0600

    tried to follow step 5 but had more issues similar to the last class

--- prepare/Learning04/Writing.cs
+++ prepare/Learning04/Writing.cs
@@ -1,2 +1,22 @@
 using System;
 
+// Added math to inherit from Assignments
+public class Writing Writing : Assignments
+{
+    // Added private variable
+    private string _title;
+
+    // Wrote the constructor
+    public Writing(string studentName, string topic, string title)
+    {
+        _title = title;
+    }
+
+    // Added the getwritinginfo method and have it return the title and by student name
+    public string GetWritingInfo()
+    {
+        string studentsName = GetStudentsName();
+        return $"{_title} by {studentsName}";
+    }
+
+}
