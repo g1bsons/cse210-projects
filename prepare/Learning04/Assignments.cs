b8fe28aa9159380f77eeadb8a6f951a794d90072
gibson <gibsoncsmith1@gmail.com>
2026-06-30 17:22:01 -0600

    wrote the assignment class according to the step 3 in the learn

--- prepare/Learning04/Assignments.cs
+++ prepare/Learning04/Assignments.cs
@@ -1,2 +1,22 @@
 using System;
 
+public class Assignments
+{
+    // Added the attributes as private variables
+    private string _studentName;
+    private string _topic;
+
+    // Added the constructor to take in a students name and topic
+    public Assignments (string studentsName, string topic)
+    {
+        _studentName = studentsName;
+        _topic = topic;
+    }
+
+    // Added the GetSummary method to return a name and topic with a dash
+    public string GetSummary()
+    {
+        return _studentName + " - " + _topic;
+    }
+
+}
