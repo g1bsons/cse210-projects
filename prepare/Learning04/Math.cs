96b95bd4cf68314e314bf5a4b6b1f68c810fa118
gibson <gibsoncsmith1@gmail.com>
2026-06-30 17:47:10 -0600

    added the constructor and method for the math class, idk why its throwing errors though cvause the code looks correct

--- prepare/Learning04/Math.cs
+++ prepare/Learning04/Math.cs
@@ -7,7 +7,17 @@ public class Math Math : Assignments
     private string _textbookSection;
     private string _problems;
 
+    // Created the constructor for all 4 parameters
+    public Math(string studentName, string topic, string textbookSection, string problems)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
 
-
+    // Added a Gethomework method to return the hw assignments
+    public string GetHomeworkList()
+    {
+        return $"Section: {_textbookSection} Problems: {_problems}";
+    }
 
 }
