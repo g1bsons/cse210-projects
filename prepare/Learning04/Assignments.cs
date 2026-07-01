ab0a87eec59a66a77e9719c83b017e85f812209e
gibson <gibsoncsmith1@gmail.com>
2026-06-30 18:12:17 -0600

    cleaned up my variable names and added more consistency across all the methods

--- prepare/Learning04/Assignments.cs
+++ prepare/Learning04/Assignments.cs
@@ -3,20 +3,20 @@ using System;
 public class Assignments
 {
     // Added the attributes as private variables
-    private string _studentName;
+    private string _studentsName;
     private string _topic;
 
     // Added the constructor to take in a students name and topic
     public Assignments (string studentsName, string topic)
     {
-        _studentName = studentsName;
+        _studentsName = studentsName;
         _topic = topic;
     }
 
     // added these lines from the xample cause theres lots of issues in the code
-    public string GetStudentName()
+    public string GetStudentsName()
     {
-        return _studentName;
+        return _studentsName;
     }
 
     public string GetTopic()
@@ -27,7 +27,7 @@ public class Assignments
     // Added the GetSummary method to return a name and topic with a dash
     public string GetSummary()
     {
-        return _studentName + " - " + _topic;
+        return _studentsName + " - " + _topic;
     }
 
 }
