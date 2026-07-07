86a727ed99a638bbda1cfb69b4068dca4c9efccc
gibson <gibsoncsmith1@gmail.com>
2026-07-07 17:50:55 -0600

    wrote the tests along the way and then aded the list below them with a foreach loop

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square mySquare = new Square("Purple", 4);
+        Console.WriteLine(mySquare.GetColor());
+        Console.WriteLine(mySquare.GetArea());
+
+        Rectangle myRectangle = new Rectangle("Orange", 4, 6);
+        Console.WriteLine(myRectangle.GetColor());
+        Console.WriteLine(myRectangle.GetArea());
+
+        Circle myCircle = new Circle("Green", 3);
+        Console.WriteLine(myCircle.GetColor());
+        Console.WriteLine(myCircle.GetArea());
+        
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(mySquare);
+        shapes.Add(myRectangle);
+        shapes.Add(myCircle);
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine(s.GetColor());
+            Console.WriteLine(s.GetArea());
+        }
     }
 }
