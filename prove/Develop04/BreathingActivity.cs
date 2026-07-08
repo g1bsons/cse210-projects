9e58287585c352e1f6de765fccec68cbc9ffa16c
gibson <gibsoncsmith1@gmail.com>
2026-07-07 21:34:48 -0600

    wrote a simple breathing activity class using activity as a parents class

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -1,2 +1,24 @@
 using System;
 
+public class BreathingActivity : Activity
+{
+    public BreathingActivity() : base(
+        "Breathing", "This will help you relax by focusing on your breathing and letting go of everything else.")
+    {}
+
+    protected override void DoActivity()
+    {
+        DateTime endTime = DateTime.Now.AddSeconds(_durationSec);
+
+        while (DateTime.Now < endTime)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Breathe in...");
+            ShowCountDown(5);
+
+            Console.WriteLine();
+            Console.WriteLine("Breathe out...");
+            ShowCountDown(5);
+        }
+    }
+}
