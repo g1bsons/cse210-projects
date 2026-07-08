618bd394df227edbda7ff4349d83a4f517de1058
gibson <gibsoncsmith1@gmail.com>
2026-07-07 21:38:46 -0600

    this is the main brains of the operation, the menu allows thew user to control which activity then want to complete and also gives an exit feature if they are done with the activities. It also adds how many total activites they completed after ending their session

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        bool running = true;
+        int activitiesCompeted = 0;
+
+        while (running)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine(" 1. Breathing Activity");
+            Console.WriteLine(" 2. Reflection Activity");
+            Console.WriteLine(" 3. Listing Activity");
+            Console.WriteLine(" 4. Quit");
+            Console.WriteLine("Please select a choice from the menu above");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                BreathingActivity breathing = new BreathingActivity();
+                breathing.Run();
+                activitiesCompeted++;
+            }
+            else if (choice == "2")
+            {
+                ReflectionActivity reflecting = new ReflectionActivity();
+                reflecting.Run();
+                activitiesCompeted++;
+            }
+            else if (choice == "3")
+            {
+                ListingActivity listing = new ListingActivity();
+                listing.Run();
+                activitiesCompeted++;
+            }   
+            else if (choice == "4")
+            {
+                running = false;
+            }
+            else
+            {
+                Console.WriteLine("Please select a valid option from the menu above");
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"Good job! You completed {activitiesCompeted} activity/activities this session.");
     }
 }
