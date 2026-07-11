df1a6e9fabf85f470cd8d5e7f1515d3113f68f5c
gibson <gibsoncsmith1@gmail.com>
2026-07-10 19:50:52 -0600

    started the goals class, added variables and a constructor, and some getters

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -1,1 +1,36 @@
 using System;
+
+public abstract class Goal
+{
+    //these are the variables
+    private string _name;
+    private int _points;
+    private bool _isComplete;
+
+
+    //this is the constructor
+    public Goal(string name, int points)
+    {
+        _name = name;
+        _points = points;
+        _isComplete = false;
+    }
+
+
+    //these are the getters
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public int GetPoints()
+    {
+        return _points;
+    }
+
+    public bool IsComplete()
+    {
+        return _isComplete;
+    }
+
+}
