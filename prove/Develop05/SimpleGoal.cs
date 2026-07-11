f33601a58d54d59346b82259cb48d8370fc101fe
gibson <gibsoncsmith1@gmail.com>
2026-07-11 11:10:56 -0600

    wrote in all of the abstract classes into simplegoals

--- prove/Develop05/SimpleGoal.cs
+++ prove/Develop05/SimpleGoal.cs
@@ -6,4 +6,38 @@ public class SimpleGoal : Goal
     public SimpleGoal(string name, int points) : base(name, points)
     {}
 
+
+    // uses the abstract method from 'Goal' to orcerride 'RecordEvent'
+    public override int RecordEvent()
+    {
+        if (IsComplete())
+        {
+            return 0;
+        }
+
+        SetComplete(true);
+        return GetPoints();
+
+    }
+
+    // uses the abstract method from 'Goal' to override 'GetDetails'
+    public override string GetDetails()
+    {
+        string mark;
+        if (IsComplete())
+        {
+            mark = "[X]";
+        }
+        else
+        {
+            mark = "[ ]";
+        }
+        return mark + " " + GetName();
+    }
+
+    // uses the abstract method from 'Goal' to override 'SaveString'
+    public override string SaveString()
+    {
+        return "SimpleGoal:" + GetName() + ":" + GetPoints() + ":" + IsComplete();
+    }
 }
